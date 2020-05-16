63ad139d720f3e108a1a89d74baf89470b5b37a1
Anna Gladzińska <agladzinska@gmail.com>
2020-05-16 11:16:37 +0200

    Add tests for formatFullname

--- utils/formatFullname.js
+++ utils/formatFullname.js
@@ -1,4 +1,7 @@
 module.exports = (fullName) => {
+  if(!fullName) return 'Error';
+  if(typeof fullName !== 'string') return 'Error';
+  if(fullName.split().length === 3 || fullName.split().length === 1) return 'Error';
   const [ firstName, lastName ] = fullName.split(' ');
   if(!firstName || !lastName) return false;
   return firstName[0].toUpperCase + firstName.slice(1).toLowerCase() + lastName[0].toUpperCase + lastName.slice(1).toLowerCase() ;
