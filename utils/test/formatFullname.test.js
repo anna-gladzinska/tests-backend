7428dad297b24452fcb30771996ea180a7ea2010
Anna Gladzińska <agladzinska@gmail.com>
2020-05-17 16:39:59 +0200

    Fix test for number of words

--- utils/test/formatFullname.test.js
+++ utils/test/formatFullname.test.js
@@ -3,20 +3,20 @@ const expect = require('chai').expect;
 
 describe('FormatFullname', () => {
 
-    it('should return an error if "content" is empty', () => {
-        expect(formatFullname(null)).to.equal('Error');
-      });
-
-      it('should return an error if "content" arg is not a string', () => {
-        expect(formatFullname(undefined)).to.equal('Error');
-        expect(formatFullname(12)).to.equal('Error');
-        expect(formatFullname({})).to.equal('Error');
-        expect(formatFullname([])).to.equal('Error');
-        expect(formatFullname(function() {})).to.equal('Error');
-      });
-
-      it('should return an error if there is something more or less than firstname and lastname', () => {
-        expect(formatFullname('John Doe Test')).to.equal('Error');
-        expect(formatFullname('John')).to.equal('Error');
-      });
+  it('should return an error if "content" is empty', () => {
+    expect(formatFullname(null)).to.equal('Error');
   });
+
+  it('should return an error if "content" arg is not a string', () => {
+    expect(formatFullname(undefined)).to.equal('Error');
+    expect(formatFullname(12)).to.equal('Error');
+    expect(formatFullname({})).to.equal('Error');
+    expect(formatFullname([])).to.equal('Error');
+    expect(formatFullname(function () {})).to.equal('Error');
+  });
+
+  it('should return an error if there is something more or less than firstname and lastname', () => {
+    expect(formatFullname('John Doe Test')).to.equal('Error');
+    expect(formatFullname('John')).to.equal('Error');
+  });
+});
