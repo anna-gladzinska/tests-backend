1d776a4d2f1a4330c661a86b7b79a166dc2be528
Anna Gladzińska <agladzinska@gmail.com>
2020-05-20 13:02:11 +0200

    Minor fix

--- utils/formatFullname.js
+++ utils/formatFullname.js
@@ -1,7 +1,7 @@
 module.exports = (fullName) => {
   if (!fullName) return 'Error';
   if (typeof fullName !== 'string') return 'Error';
-  if (fullName.split(/\W+/).length >= 3 || fullName.split(/\W+/).length === 1) return 'Error';
+  if (fullName.split(/\W+/).length !== 2) return 'Error';
   const [firstName, lastName] = fullName.split(' ');
   if (!firstName || !lastName) return false;
   return firstName[0].toUpperCase + firstName.slice(1).toLowerCase() + lastName[0].toUpperCase + lastName.slice(1).toLowerCase();
